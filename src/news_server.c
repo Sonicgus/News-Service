bc4d2c057002a5b72e6e61c28f4236f33f505306
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-04-13 20:51:37 +0100

    Update news_server.c

--- src/news_server.c
+++ src/news_server.c
@@ -6,7 +6,7 @@ typedef struct user_node
 {
     char username[21];
     char password[21];
-    char type[14];
+    char type[15];
     struct user_node *next;
 } UserNode;
 
@@ -22,6 +22,13 @@ int main(int argc, char *argv[])
         exit(1);
     }
     read_config_file(argv[3]);
+    UserNode *head = root;
+    for (head = root; head != NULL; head = head->next)
+    {
+        printf("Username: %s\n", head->username);
+        printf("Password: %s\n", head->password);
+        printf("Type: %s\n\n", head->type);
+    }
 
     return 0;
 }
@@ -29,10 +36,10 @@ int main(int argc, char *argv[])
 void read_config_file(const char *config_file)
 {
     FILE *fp;
-    UserNode *new_node, head = root;
+    UserNode *new_node, **head = &root;
     char line[100];
 
-    fp = fopen("usuarios.txt", "r");
+    fp = fopen(config_file, "r");
 
     if (fp == NULL)
     {
@@ -40,7 +47,7 @@ void read_config_file(const char *config_file)
         exit(1);
     }
 
-    while (fgets(line, sizeof(line), file))
+    while (fgets(line, sizeof(line), fp))
     {
         new_node = (UserNode *)malloc(sizeof(UserNode));
 
@@ -49,7 +56,8 @@ void read_config_file(const char *config_file)
         strcpy(new_node->type, strtok(NULL, "\n"));
         new_node->next = NULL;
 
-        head = new_node->next;
+        *head = new_node;
+        head = &new_node->next;
     }
 
     fclose(fp);
