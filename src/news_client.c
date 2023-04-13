f750ace7c210008aee318b4b3994437b8b196bf7
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-04-13 20:22:56 +0100

    Update news_client.c

--- src/news_client.c
+++ src/news_client.c
@@ -2,5 +2,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        printf("news_client {endereço do servidor} {PORTO_NOTICIAS}\n");
+        exit(1);
+    }
+
     return 0;
 }
