690c030ab9f372b7d15f8308999d2d6165902f78
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-05-21 19:07:10 +0100

    Update news_client.c

--- src/news_client.c
+++ src/news_client.c
@@ -35,8 +35,6 @@ void erro(char *msg);
 
 void *multicast(void *topic_sub)
 {
-    printf("teste\n");
-    fflush(stdout);
     Subscription *sub = ((Subscription *)(topic_sub));
     ///////////////////////////////////////////
     struct sockaddr_in addr;
