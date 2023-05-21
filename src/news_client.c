d6c536bbcc810f80f07592d0ca141b4b1c2f0d4c
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-05-21 16:36:51 +0100

    Update news_client.c

--- src/news_client.c
+++ src/news_client.c
@@ -5,10 +5,12 @@
 #include <string.h>
 #include <pthread.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 // tamanho do buffer
 #define BUF_SIZE 1024
